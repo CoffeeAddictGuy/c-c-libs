4288d87ae1bb488b39e98a421fd8518d1723d12b
CoffeeAddictGuy <richcrwo@gmail.com>
2025-09-17 15:56:21 +0300

    my sort algorithm lib

---
+++ mysortlib.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void bubbleSort(int* array, int arraySize) {
+    int swapped = 1;
+    while (swapped)
+    {
+        swapped = 0;
+        for (int i = 1; i < arraySize; i++) {
+            if (array[i] < array[i-1]) {
+                swap(&array[i], &array[i-1]);
+                swapped = 1;
+            }
+        }
+    }
+}
+
+void gnomeSort(int* array, int arraySize) {
+    int i = 1;
+    int j = 2;
+    while (i < arraySize) {
+        if (array[i - 1] < array[i]){
+            i = j;
+            j += 1;
+        }
+        else {
+            swap(&array[i - 1], &array[i]);
+            i = i - 1;
+            if (i == 0) {
+                i = j;
+                j += 1;
+            }
+        }
+    }
+}
